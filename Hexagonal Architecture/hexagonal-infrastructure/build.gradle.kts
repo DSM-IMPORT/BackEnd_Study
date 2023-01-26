4d958f34de2481f54b49eacf193a57b5fde0e859
주영 <jyk102999@dsm.hs.kr>
2023-01-26 17:41:13 +0900

    ⚙️ :: 의존성 추가

--- Hexagonal Architecture/hexagonal-infrastructure/build.gradle.kts
+++ Hexagonal Architecture/hexagonal-infrastructure/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.springframework.boot") version "3.0.0"
+    id("org.springframework.boot") version "2.7.7"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
@@ -10,6 +10,8 @@ dependencies {
     implementation(project(":hexagonal-application"))
     implementation(project(":hexagonal-domain"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     runtimeOnly("com.mysql:mysql-connector-j")
 }
 
