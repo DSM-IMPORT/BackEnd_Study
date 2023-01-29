4398bcc584effb5c5793f3e3a65c263c86cb4ba8
주영 <jyk102999@dsm.hs.kr>
2023-01-29 18:53:39 +0900

    ⚡️ :: 안 쓰는 import 삭제

--- Hexagonal Architecture/hexagonal-application/src/main/kotlin/com/example/domain/user/spi/UserJwtPort.kt
+++ Hexagonal Architecture/hexagonal-application/src/main/kotlin/com/example/domain/user/spi/UserJwtPort.kt
@@ -1,7 +1,6 @@
 package com.example.domain.user.spi
 
 import com.example.domain.auth.dto.response.TokenResponse
-import java.util.*
 
 interface UserJwtPort {
     fun receiveToken(accountId: String): TokenResponse
