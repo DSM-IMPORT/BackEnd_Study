f613d65d6b3fb860fe5d0fd37d3753b2768b1294
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-06 00:27:44 +0900

    🔍::Int -> HttpStatus

--- Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/domain/common/error/DomainErrorCode.kt
+++ Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/domain/common/error/DomainErrorCode.kt
@@ -1,20 +1,21 @@
 package com.example.domain.common.error
 
 import com.example.global.error.ErrorProperty
+import org.springframework.http.HttpStatus
 
 enum class DomainErrorCode(
-    private val status: Int,
+    private val status: HttpStatus,
     private val message: String
 ): ErrorProperty {
 
     //400
-    DUPLICATED_MEMBER(400, ""),
-    INCORRECT_PASSWORD(400, ""),
+    DUPLICATED_MEMBER(HttpStatus.BAD_REQUEST, ""),
+    INCORRECT_PASSWORD(HttpStatus.BAD_REQUEST, ""),
 
     //404
-    USER_NOT_FOUND(404, "")
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "")
     ;
 
-    override fun status(): Int = status
+    override fun status(): Int = status.value()
     override fun message(): String = message
 }
