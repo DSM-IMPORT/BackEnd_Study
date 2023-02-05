4002d09cbcd6e05221d3d5f70eaad60512c0bbe9
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-06 00:13:04 +0900

    🔧::BCryptPasswordEncoder

--- Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/domain/user/mapper/UserMapper.kt
+++ Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/domain/user/mapper/UserMapper.kt
@@ -2,17 +2,20 @@ package com.example.domain.user.mapper
 
 import com.example.domain.user.entity.UserJpaEntity
 import com.example.domain.user.model.User
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Component
 
 @Component
-class UserMapper {
+class UserMapper(
+    private val passwordEncoder: BCryptPasswordEncoder
+) {
 
     fun toEntity(user: User): UserJpaEntity {
 
         return UserJpaEntity(
             user.id,
             user.accountId,
-            user.password,
+            passwordEncoder.encode(user.password),
             user.name,
             user.age
         )
