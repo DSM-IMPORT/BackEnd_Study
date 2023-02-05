49dab09d71cd29a92a76236a68d2b38080704c62
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-05 23:22:30 +0900

    🔍::Refactoring

--- Hexagonal Architecture/hexagonal-presentation/src/main/kotlin/com/example/domain/user/dto/SignupWebRequest.kt
+++ Hexagonal Architecture/hexagonal-presentation/src/main/kotlin/com/example/domain/user/dto/SignupWebRequest.kt
@@ -9,18 +9,18 @@ import jakarta.validation.constraints.Size
 
 data class SignupWebRequest(
     @field:NotBlank
-    @Size(min = 10, max = 20, message = "account_id는 최대 20자입니다.")
+    @field:Size(min = 10, max = 20, message = "account_id는 최대 20자입니다.")
     val accountId: String,
 
     @field:NotBlank
     @field:Pattern(
         regexp = PasswordRegexp.REGEX,
-        message = "정규식 지키셈"
+        message = "8자 ~ 16자, 하나 이상의 문자, 하나의 이상의 숫자 및 하나의 이상의 특수 문자"
     )
     val password: String,
 
     @field:NotBlank
-    @field:Max(5)
+    @field:Size(min = 2, max = 5, message = "이름은 2 ~ 5 글자 입니다")
     val name: String,
 
     @field:NotNull
