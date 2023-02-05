ec30bb57e80423535f0ab1ed75194bbd7027349a
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-06 00:28:55 +0900

    🔍::jakarta- > javax

--- Hexagonal Architecture/hexagonal-presentation/src/main/kotlin/com/example/domain/user/UserWebAdapter.kt
+++ Hexagonal Architecture/hexagonal-presentation/src/main/kotlin/com/example/domain/user/UserWebAdapter.kt
@@ -3,18 +3,14 @@ package com.example.domain.user
 import com.example.domain.auth.dto.response.TokenResponse
 import com.example.domain.user.dto.LoginRequest
 import com.example.domain.user.dto.LoginWebRequest
-import com.example.domain.user.dto.SignupWebRequest
 import com.example.domain.user.dto.SignupRequest
+import com.example.domain.user.dto.SignupWebRequest
 import com.example.domain.user.usecase.LoginUseCase
 import com.example.domain.user.usecase.SignupUseCase
-import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @Validated
 @RestController
