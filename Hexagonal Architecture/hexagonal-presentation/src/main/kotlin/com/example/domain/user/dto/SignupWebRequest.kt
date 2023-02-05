7acf182cb920e70cbf0a84c03b3562b385f1dada
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-06 00:13:59 +0900

    🔍::RF

--- Hexagonal Architecture/hexagonal-presentation/src/main/kotlin/com/example/domain/user/dto/SignupWebRequest.kt
+++ Hexagonal Architecture/hexagonal-presentation/src/main/kotlin/com/example/domain/user/dto/SignupWebRequest.kt
@@ -1,13 +1,13 @@
 package com.example.domain.user.dto
 
 import com.example.global.regex.PasswordRegexp
-import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 
 data class SignupWebRequest(
+
     @field:NotBlank
     @field:Size(min = 10, max = 20, message = "account_id는 최대 20자입니다.")
     val accountId: String,
