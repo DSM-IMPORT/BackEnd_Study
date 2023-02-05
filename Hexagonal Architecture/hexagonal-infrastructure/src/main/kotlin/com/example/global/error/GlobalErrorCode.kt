cbe7bc7034c17020ab435db4cafb93d8ab68295e
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-05 23:24:38 +0900

    🔍::ErrorCode -> GlobalErrorCode & DomainErrorCode

--- Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/global/error/GlobalErrorCode.kt
+++ Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/global/error/GlobalErrorCode.kt
@@ -1,13 +1,15 @@
 package com.example.global.error
 
-enum class ErrorCode(
-    val status: Int,
-    val message: String
+enum class GlobalErrorCode(
+    private val status: Int,
+    private val message: String
 ): ErrorProperty {
+
     BAD_REQUEST(400, "Bad Request"),
     INVALID_TOKEN(401, "Invalid Token"),
     EXPIRED_TOKEN(401, "Expired Token"),
-    INTERNEL_SERVER_ERROR(500, "Internel Server Error");
+    INTERNAL_SERVER_GLOBAL_ERROR(500, "Internal Server Error")
+    ;
 
     override fun status(): Int = status
     override fun message(): String = message
