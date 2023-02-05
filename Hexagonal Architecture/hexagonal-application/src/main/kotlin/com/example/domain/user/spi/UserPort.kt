b5ad059fea2f49c89ba13ea41547fccf98b78352
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-06 00:12:13 +0900

    🔧::verifyUser

--- Hexagonal Architecture/hexagonal-application/src/main/kotlin/com/example/domain/user/spi/UserPort.kt
+++ Hexagonal Architecture/hexagonal-application/src/main/kotlin/com/example/domain/user/spi/UserPort.kt
@@ -1,7 +1,11 @@
 package com.example.domain.user.spi
 
+import com.example.domain.user.dto.LoginRequest
 import com.example.domain.user.model.User
 
 interface UserPort {
+
     fun saveUser(user: User)
+
+    fun verifyUser(loginRequest: LoginRequest): String
 }
