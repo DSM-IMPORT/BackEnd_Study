47db576ac7e56ea03bd9a3a106a5c0aefa59333f
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-05 23:28:26 +0900

    🔍:: model.User -> entity.UserJpaEntity

--- Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/global/config/security/principle/AuthDetails.kt
+++ Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/global/config/security/principle/AuthDetails.kt
@@ -1,15 +1,15 @@
-package com.example.global.security.principle
+package com.example.global.config.security.principle
 
-import com.example.domain.user.model.User
+import com.example.domain.user.entity.UserJpaEntity
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 class AuthDetails(
-    private val user: User
+    private val user: UserJpaEntity
 ) : UserDetails {
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf()
 
-    override fun getPassword(): String? = null
+    override fun getPassword(): String = user.password
 
     override fun getUsername(): String = user.accountId
 
