d0c06a62732dc301ce98017b360644ad73f7abd8
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-05 23:35:25 +0900

    🔍::REFACTORING

--- Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/domain/user/UserPersistenceAdapter.kt
+++ Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/domain/user/UserPersistenceAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.domain.user
 
+import com.example.domain.user.exception.DuplicatedMemberException
 import com.example.domain.user.mapper.UserMapper
 import com.example.domain.user.repository.UserJpaRepository
 import com.example.domain.user.model.User
@@ -11,7 +12,13 @@ class UserPersistenceAdapter(
     private val userJpaRepository: UserJpaRepository,
     private val userMapper: UserMapper
 ): UserPort {
+
     override fun saveUser(user: User) {
+
+        if(userJpaRepository.existsByAccountId(user.accountId)) {
+            throw DuplicatedMemberException.EXCEPTION
+        }
+
         userJpaRepository.save(userMapper.toEntity(user))
     }
 }
