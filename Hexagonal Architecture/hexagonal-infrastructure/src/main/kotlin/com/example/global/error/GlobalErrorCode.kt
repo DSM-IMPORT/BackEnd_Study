3cb59af1e50934cf56f3cdfcb5fe86ff130aeef4
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-06 00:27:55 +0900

    🔍::Int -> HttpStatus

--- Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/global/error/GlobalErrorCode.kt
+++ Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/global/error/GlobalErrorCode.kt
@@ -1,16 +1,18 @@
 package com.example.global.error
 
+import org.springframework.http.HttpStatus
+
 enum class GlobalErrorCode(
-    private val status: Int,
+    private val status: HttpStatus,
     private val message: String
 ): ErrorProperty {
 
-    BAD_REQUEST(400, "Bad Request"),
-    INVALID_TOKEN(401, "Invalid Token"),
-    EXPIRED_TOKEN(401, "Expired Token"),
-    INTERNAL_SERVER_GLOBAL_ERROR(500, "Internal Server Error")
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "Bad Request"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid Token"),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "Expired Token"),
+    INTERNAL_SERVER_GLOBAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error")
     ;
 
-    override fun status(): Int = status
+    override fun status(): Int = status.value()
     override fun message(): String = message
 }
