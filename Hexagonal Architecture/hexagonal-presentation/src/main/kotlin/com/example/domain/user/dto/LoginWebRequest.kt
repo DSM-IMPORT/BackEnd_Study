438daede4d15c60677947557294dbc8d51f7c507
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-06 00:10:26 +0900

    📌::LoginWebRequest

---
+++ Hexagonal Architecture/hexagonal-presentation/src/main/kotlin/com/example/domain/user/dto/LoginWebRequest.kt
@@ -0,0 +1,20 @@
+package com.example.domain.user.dto
+
+import com.example.global.regex.PasswordRegexp
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
+
+data class LoginWebRequest(
+
+    @field:NotBlank
+    @field:Size(min = 10, max = 20, message = "account_id는 최대 20자입니다.")
+    val accountId: String,
+
+    @field:NotBlank
+    @field:Pattern(
+        regexp = PasswordRegexp.REGEX,
+        message = "8자 ~ 16자, 하나 이상의 문자, 하나의 이상의 숫자 및 하나의 이상의 특수 문자"
+    )
+    val password: String
+)
