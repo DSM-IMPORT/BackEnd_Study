05cfcb38af5869e5b9d13a9cfbd9adaeed21cf51
이하성 <tedsoftj1123@gmail.com>
2023-02-05 22:35:25 +0900

    📑 :: 마이페이지 내 피드 조회 추가

--- Hexagonal Architecture/hexagonal-application/src/main/kotlin/com/example/domain/user/dto/response/QueryUserMyInfoResponse.kt
+++ Hexagonal Architecture/hexagonal-application/src/main/kotlin/com/example/domain/user/dto/response/QueryUserMyInfoResponse.kt
@@ -3,5 +3,12 @@ package com.example.domain.user.dto.response
 data class QueryUserMyInfoResponse(
     val accountId: String,
     val name: String,
-    val age: Int
-)
+    val age: Int,
+    val feeds: List<FeedResponse>
+) {
+    data class FeedResponse(
+        val feedId: Long,
+        val title: String
+    )
+}
+
--- Hexagonal Architecture/hexagonal-application/src/main/kotlin/com/example/domain/user/usecase/QueryMyInfoUseCase.kt
+++ Hexagonal Architecture/hexagonal-application/src/main/kotlin/com/example/domain/user/usecase/QueryMyInfoUseCase.kt
@@ -1,5 +1,7 @@
 package com.example.domain.user.usecase
 
+import com.example.domain.feed.spi.FeedPort
+import com.example.domain.user.dto.response.QueryUserMyInfoResponse.FeedResponse
 import com.example.domain.user.dto.response.QueryUserMyInfoResponse
 import com.example.domain.user.exception.UserNotFoundException
 import com.example.domain.user.spi.UserPort
@@ -9,17 +11,26 @@ import com.example.global.annotation.UseCase
 @UseCase
 class QueryMyInfoUseCase(
     private val userPort: UserPort,
+    private val feedPort: FeedPort,
     private val userSecurityPort: UserSecurityPort
 ) {
 
     fun execute(): QueryUserMyInfoResponse {
         val userId = userSecurityPort.getCurrentUserId()
         val user = userPort.findByAccountId(userId)?: throw UserNotFoundException
+        val feeds = feedPort.findAllByUserId(user.id!!)
+            .map {
+                FeedResponse(
+                    title = it.title,
+                    feedId = it.id!!
+                )
+            }
 
         return QueryUserMyInfoResponse(
             accountId = user.accountId,
             name = user.name,
-            age = user.age
+            age = user.age,
+            feeds
         )
     }
 }
