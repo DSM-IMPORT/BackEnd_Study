77a2a4154e965f3dcd25d129f329c72995418ec4
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-06 00:13:43 +0900

    🔍::RF

--- Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/domain/user/exception/DuplicatedMemberException.kt
+++ Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/domain/user/exception/DuplicatedMemberException.kt
@@ -4,6 +4,7 @@ import com.example.domain.common.error.DomainErrorCode
 import com.example.global.error.CustomException
 
 class DuplicatedMemberException : CustomException(DomainErrorCode.DUPLICATED_MEMBER) {
+
     companion object {
         @JvmField
         val EXCEPTION = DuplicatedMemberException()
