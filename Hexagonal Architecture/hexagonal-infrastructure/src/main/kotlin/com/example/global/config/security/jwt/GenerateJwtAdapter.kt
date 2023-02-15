3ae60408a660f750a2cb7f267752c687077c0b15
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-15 19:31:20 +0900

    🗑️ :: reissue

--- Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/global/config/security/jwt/GenerateJwtAdapter.kt
+++ Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/global/config/security/jwt/GenerateJwtAdapter.kt
@@ -2,13 +2,12 @@ package com.example.global.config.security.jwt
 
 import com.example.domain.auth.dto.response.TokenResponse
 import com.example.domain.auth.spi.JwtPort
+import com.example.global.config.security.jwt.dotenv.JwtProperties
 import com.example.global.persistence.auth.entity.RefreshTokenEntity
 import com.example.global.persistence.auth.repository.RefreshTokenRepository
-import com.example.global.config.security.jwt.dotenv.JwtProperties
-import com.example.global.exception.ExpiredTokenException
-import io.jsonwebtoken.*
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 @Component
@@ -23,18 +22,6 @@ class GenerateJwtAdapter(
         generateRefreshToken(accountId)
     )
 
-    @Transactional
-    override fun reissue(rfToken: String): TokenResponse {
-
-        val rf = refreshTokenRepository.findByToken(rfToken)
-            ?: throw ExpiredTokenException.EXCEPTION
-
-        val response = receiveToken(rf.token)
-
-        refreshTokenRepository.delete(rf)
-
-        return response
-    }
 
     private fun generateRefreshToken(accountId: String): String {
 
