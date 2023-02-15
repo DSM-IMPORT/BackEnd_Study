a3f1644ab45b459bbc16e5aa576e550fa393c44d
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-15 19:29:35 +0900

    🔧 :: error message

--- Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/domain/common/error/DomainErrorCode.kt
+++ Hexagonal Architecture/hexagonal-infrastructure/src/main/kotlin/com/example/domain/common/error/DomainErrorCode.kt
@@ -9,11 +9,11 @@ enum class DomainErrorCode(
 ): ErrorProperty {
 
     //400
-    DUPLICATED_MEMBER(HttpStatus.BAD_REQUEST, ""),
-    INCORRECT_PASSWORD(HttpStatus.BAD_REQUEST, ""),
+    DUPLICATED_MEMBER(HttpStatus.BAD_REQUEST, "Duplicated Member"),
+    INCORRECT_PASSWORD(HttpStatus.BAD_REQUEST, "Incorrect Password"),
 
     //404
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "")
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User Not Found")
     ;
 
     override fun status(): Int = status.value()
